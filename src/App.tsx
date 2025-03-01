39b7dd8008c3bf27940df0aea4f862ed2f8187fa
k-tsunoda615 <kntrtnd@gmail.com>
2025-03-01 19:31:44 +0900

    husky動作テスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <p className="mt-10 text-[30px] text-red-500 bg-blue-100 underline underline-offset-4">
         Hello tailwind4!
       </p>
+      <p>huskyテスト</p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -32,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
