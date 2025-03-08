724b9de09ea40566bdcdaff3dba8f880ccd76cd4
k-tsunoda615 <kntrtnd@gmail.com>
2025-03-08 19:51:31 +0900

    feat: threadsページへのrooting追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ThreadsListPage } from "./pages/ThreadsListPage/index";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <p className="mt-10 text-[30px] text-red-500 bg-blue-100 underline underline-offset-4">
-        Hello tailwind4!
-      </p>
-      <p>huskyテスト</p>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/threads" />} />
+      <Route path="/threads" element={<ThreadsListPage />} />
+      {/* <Route path="/threads/:id" element={<ThreadDetailPage />} /> */}
+    </Routes>
   );
 }
 
